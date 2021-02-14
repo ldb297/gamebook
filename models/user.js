900f175b53a3dccd17c66e6debd8bee80e132447
Leonidas <ldbrooks297@gmail.com>
2021-02-14 13:24:58 -0800

    auth complete w/ updated user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 'use strict';
+const bcrypt = require('bcrypt')
 const {
   Model
 } = require('sequelize');
@@ -16,12 +17,52 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   user.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [4, 24],
+          msg: 'username must be between 4 and 24 characters'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'invalid email'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8, 30],
+          msg: 'password must be between 8 and 30 characters'
+        }
+      }
+    }
   }, {
     sequelize,
     modelName: 'user',
   });
+
+user.addHook('beforeCreate', (newUser) =>{
+  let hash = bcrypt.hashSync(newUser.password, 6)
+  newUser.password = hash
+})
+
+user.prototype.validPassword = function(passedword) {
+  let isCorrectPassword = bcrypt.compareSync(passedword, this.password)
+  return isCorrectPassword
+}
+
+user.prototype.toJSON = function(){
+  let userData = this.get()
+  delete userData.password
+  return userData
+}
+
   return user;
 };
