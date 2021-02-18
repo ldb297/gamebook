a0a61177fd860b0be57934a4b01eb07d8ffb0de0
Leonidas <ldbrooks297@gmail.com>
2021-02-18 11:50:10 -0800

    isLoggedIn.js: added 404 routing

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,7 +1,7 @@
 function isLoggedIn(req, res, next){
     if(!req.user){
         req.flash('error', 'You must be signed in to access page')
-        res.redirect('/auth/login')
+        res.redirect('/error/404')
     } else {
         next()
     }
